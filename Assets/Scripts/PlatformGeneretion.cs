7c1d38caf340e8b629a15d799ec4934dfc835aad
FabricioOliveirdads <fabricio_alvesoliveira1@hotmail.com>
2020-12-14 21:40:31 -0300

    First commit 14122020

---
+++ Assets/Scripts/PlatformGeneretion.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGeneretion : MonoBehaviour
+{
+    public GameObject Platform;
+    public Transform point;
+    public float Distance;
+    private float plaftormWidth;
+    void Start()
+    {
+        if (Platform.GetComponent<BoxCollider2D>())
+        {
+            plaftormWidth = Platform.GetComponent<BoxCollider2D>().size.x;
+        }
+        else
+        {
+            plaftormWidth = Platform.GetComponent<PolygonCollider2D>().bounds.size.x;
+        }
+    }
+
+    
+    void Update()
+    {
+        if(transform.position.x < point.position.x)
+        {
+           // Distance = Random.Range(3f,8f);
+            transform.position = new Vector3(transform.position.x + plaftormWidth + Distance, transform.position.y, transform.position.z);
+            Instantiate(Platform, transform.position, transform.rotation);
+        }
+    }
+}
