4b4ee0062b210331beee5fb34ae16fb88ca1ae53
FabricioOliveirdads <fabricio_alvesoliveira1@hotmail.com>
2020-12-16 22:01:27 -0300

    Spawn Enemies

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    
+    private Transform backPoint;
+    private Animator animator;
+    private Rigidbody2D rig;
+    public float minSpeed;
+    public float maxSpeed;
+
+     void Start()
+    {
+        backPoint = GameObject.Find("BackPoint").GetComponent<Transform>();
+        animator = GetComponent<Animator>();
+        rig = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // transform.Translate(Vector3.left * Speed * Time.deltaTime);
+        rig.velocity = new Vector2(Random.Range(minSpeed,maxSpeed)*-1, rig.velocity.y);
+        if(transform.position.x < backPoint.position.x)
+        {
+            Destroy(gameObject);
+        }
+    }
+     void OnTriggerEnter2D(Collider2D collision)
+    {
+       if(collision.gameObject.tag =="Bullet")
+        {
+            animator.SetTrigger("destroy");
+            Destroy(gameObject,1f);
+        } 
+    }
+}
